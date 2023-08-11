fafa6c35781aa3c41ec0c26e8c299323ed717fae
riThvik <rahulsingh995871@gmail.com>
2023-08-11 15:07:24 +0530

    erwrrwrewrer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,45 @@
 
-import './Navbar.css'
+
 import logo from '../images/logo.png'
+import is from '../images/logo-black.png'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import './Navbar.css'
+
+
 function Navbar() {
+  const [sticky, setSticky] = useState("");
+
+  // on render, set listener
+  useEffect(() => {
+  
+    window.addEventListener("scroll", isSticky);
+    return () => {
+      window.removeEventListener("scroll", isSticky);
+    };
+  }, []);
+
+  const isSticky = () => {
+    /* Method that will fix header after a specific scrollable */
+    const scrollTop = window.scrollY;
+    const stickyClass = scrollTop >= 100 ? "is-sticky"  : "rr";
+    setSticky(stickyClass);
+    console.log(stickyClass);
+  };
+
+  const classes = ` ${sticky}`;
+
+  
     return (
-        <div>
-            <nav className="c-navbar navbar navbar-expand-lg bg-transparent">
-  <div className="container-fluid px-md-5 px-2 w-md-75 w-100">
-    <Link className="navbar-brand" to="/beatmytrip">
-      <img src={logo}/>
+        <div className={`${classes} rr`}>
+            <nav className="c-navbar navbar navbar-expand-lg p-0">
+  <div className="container-fluid px-md-5 px-0 w-md-75 w-100">
+    <Link className="navbar-brand px-2" to="/beatmytrip">
+      <img src={sticky=="is-sticky" ? is : logo}/>
     </Link>
     <button
-      className="navbar-toggler"
+      className="navbar-toggler mx-2"
       type="button"
       data-bs-toggle="collapse"
       data-bs-target="#navbarSupportedContent"
