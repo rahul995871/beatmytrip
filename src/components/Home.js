33bcffbb1bc7e8c60ae7fceb8e912d36d078dae8
riThvik <rahulsingh995871@gmail.com>
2023-09-01 13:49:52 +0530

    adsadsxassad

--- src/components/Home.js
+++ src/components/Home.js
@@ -57,21 +57,21 @@ export const TestSec = () => {
   );
 };
 
-const ReadMore = ({ children }) => {
-  const text = children;
-  const [isReadMore, setIsReadMore] = useState(true);
-  const toggleReadMore = () => {
-    setIsReadMore(!isReadMore);
-  };
-  return (
-    <p className="text">
-      {isReadMore ? text.slice(0, 150) : text}
-      <span onClick={toggleReadMore} className="read-or-hide">
-        {isReadMore ? "...read more" : " show less"}
-      </span>
-    </p>
-  );
-};
+// const ReadMore = ({ children }) => {
+//   const text = children;
+//   const [isReadMore, setIsReadMore] = useState(true);
+//   const toggleReadMore = () => {
+//     setIsReadMore(!isReadMore);
+//   };
+//   return (
+//     <p className="text">
+//       {isReadMore ? text.slice(0, 150) : text}
+//       <span onClick={toggleReadMore} className="read-or-hide">
+//         {isReadMore ? "...read more" : " show less"}
+//       </span>
+//     </p>
+//   );
+// };
 export const AboutSec = ({ toggle }) => {
   // const[bb,setBb]=useState('bg-light')
   return (
